eb470c156a2b50dfa44cd4dc7fafc9326a335add
Juan Ramon <leonespartano@gmail.com>
2023-02-14 12:09:32 -0600

    Configuracion inicial del microservicio Customer

---
+++ amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,22 @@
+package com.amigoscode.customer;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    public void registerCustomer(CustomerRegistrationRequest request){
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        customerRepository.save(customer);
+
+    }
+}
