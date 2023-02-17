ecaa5c5b6c38becb73f43a97ceef51b5a10fbf36
Juan Ramon <leonespartano@gmail.com>
2023-02-16 22:32:15 -0600

    Terminacion de los componentes basicos del modulo fraud

--- amigosservices/fraud/src/main/java/com/amigoscode/fraud/FraudController.java
+++ amigosservices/fraud/src/main/java/com/amigoscode/fraud/FraudController.java
@@ -1,9 +1,24 @@
 package com.amigoscode.fraud;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@AllArgsConstructor
 @Controller
+@RequestMapping("api/v1/fraud-check")
 public class FraudController {
+
+    private final FraudCheckService fraudCheckService;
+
+     @GetMapping(path = "{customerId}")
+    public FraudCheckResponse isFrauster(@PathVariable ("customerId") Integer customerId){
+         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+         return new FraudCheckResponse(isFraudulentCustomer);
+     }
 
 }
