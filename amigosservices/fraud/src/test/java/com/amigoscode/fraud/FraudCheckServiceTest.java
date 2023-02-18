a818a54fefdd2e5e46004e01639ce0689dcdb3bd
Juan Ramon <leonespartano@gmail.com>
2023-02-17 21:39:43 -0600

    Pruebas unitarias de FraudService

---
+++ amigosservices/fraud/src/test/java/com/amigoscode/fraud/FraudCheckServiceTest.java
@@ -0,0 +1,44 @@
+package com.amigoscode.fraud;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.then;
+
+
+class FraudCheckServiceTest {
+
+    private FraudCheckService underTest;
+
+    @Mock
+    private FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    @Captor
+    ArgumentCaptor<FraudCheckHistory> captureFraudCheckHistory;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        underTest = new FraudCheckService(fraudCheckHistoryRepository);
+    }
+
+    @Test
+    void itShouldNotReturnFraudulentCustomer() {
+        //Given
+        int customerId = 1;
+        //When
+        underTest.isFraudulentCustomer(customerId);
+        //Then
+        then(fraudCheckHistoryRepository).should().save(captureFraudCheckHistory.capture());
+
+        FraudCheckHistory fraudCheckHistoryValue = captureFraudCheckHistory.getValue();
+
+        assertThat(fraudCheckHistoryValue.getCustomerId()).isEqualTo(customerId);
+
+    }
+}
